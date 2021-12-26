30e6f238b74741ed6dec4f923601f63c3e054dba
Vishal Sharma <techvishal21@gmail.com>
2021-12-26 17:29:14 +0530

    To Make A Dummy List Example Given

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: MyTheme.darkTheam(context),
+      //   theme: MyTheme.darkTheam(context),
 
       debugShowCheckedModeBanner: false,
       //initialRoute: "/home",
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/modals/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   int days = 30;
@@ -9,20 +11,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModal.item[0]);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
-          "Catalog App",
-          style: TextStyle(color: Colors.black),
-        ),
+        title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          //child: Text("Welcome to $days days of flutter by $name"),
-          child: Text(context.runtimeType.toString()),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          //itemCount: CatalogModal.item.length,
+          //following code for dummy list for teting app if don't have data.
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              //item: CatalogModal.item[index],
+              //following code for dummy list for teting app if don't have data.
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
